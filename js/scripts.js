b08d5b4647e2f41180bb437d9db571e6303d1a43
Byron <byronblocker@gmail.com>
2017-02-24 10:27:14 -0800

    delete bad line

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
   $("form#vacationTest").submit(function(event) {
     event.preventDefault();
-    var name = $("input:text#userName").val();
+    
     var answer1 = $("input:radio[name=q1]:checked").val();
     var answer2 = $("input:radio[name=q2]:checked").val();
     var answer3 = $("input:radio[name=q3]:checked").val();
     var answer4 = $("input:radio[name=q4]:checked").val();
     var answer5 = $("input:radio[name=q5]:checked").val();
     var answer6 = $("input:radio[name=q6]:checked").val();
-    // var answer7 = $("input.q7").val();
-    // var answer8 = $("input.q8").val();
-    // var answer9 = $("input.q9").val();
-    // var answer10 = $("input.q10").val();
 
     var hawaiiPts = 0;
     var thailandPts = 0;
@@ -84,31 +80,26 @@ $(document).ready(function() {
     if (hawaiiPts === winner) {
       $('.col-md-6').slideUp();
       $('#hawaii').fadeIn();
-      alert(name + "! Remember to bring a swimsuit!");
     }
 
     if (thailandPts === winner) {
       $('.col-md-6').slideUp();
       $('#thailand').fadeIn();
-      alert("Get ready for good food, " + name + "!");
     }
 
     if (australiaPts === winner) {
       $('.col-md-6').slideUp();
       $('#australia').fadeIn();
-      alert("Crikey, " + name + "! Get ready for some fun down undah!");
     }
 
     if (alaskaPts === winner) {
       $('.col-md-6').slideUp();
       $('#alaska').fadeIn();
-      alert("Pack a jacket and binoculars, " + name + ", it'll be cold but beautiful!");
     }
 
     if (europePts === winner) {
       $('.col-md-6').slideUp();
       $('#europe').fadeIn();
-      alert("It's the classic vacation, " + name + " - I'm sure you'll have fun!");
     }
   });
 });

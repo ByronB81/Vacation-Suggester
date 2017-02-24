3349104827109a5f256cef0f24d1db5e8d1e1d08
Byron <byronblocker@gmail.com>
2017-02-24 09:16:53 -0800

    finally get the inputs working correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,24 @@
-// $("#blanks form").submit(function(event) {
-//   var climateInput = ()"#preferredTemp").val();
-//   console.log(climateInput);
-//   event.preventDefault();
-// });
-
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+    console.log();
+    var childrenInput= $("input#children").val();
     var climateInput = $("input#climate").val();
     var activityInput = $("input#activity").val();
-    var pricingInput= $("input#pricing").val();
-    var varietyInput = $("input#variety").val();
-    var nameInput = $("input#name").val();
-    var addressInput = $("input#address").val();
-    var emailInput = $("input#email").val();
-    var childrenInput = $("input#children").val();
+    var pricingInput = $("input#pricing").val();
+    var exoticInput = $("input#exotic").val();
 
-    $(.name).text(nameInput);
-    // $(".person1").text(person1Input);
-    // $(".person2").text(person2Input);
-    // $(".animal").text(animalInput);
-    // $(".exclamation").text(exclamationInput);
-    // $(".verb").text(verbInput);
-    // $(".noun").text(nounInput);
-    //
-    // $("#story").show();
-    console.log("hi")
+    $(".name").text(nameInput);
+    $(".email").text(emailInput);
+    $(".children").text(childrenInput);
+    $(".climate").text(climateInput);
+    $(".activity").text(activityInput);
+    $(".pricing").text(pricingInput);
+    $(".exotic").text(exoticInput);
+
+    $("#story").show();
+
     event.preventDefault();
   });
 });
